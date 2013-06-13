b56c9d953f9f531f8d56cb1e3509de088c1095b8
Cian Booth <thisiscian@gmail.com>
2013-06-13 21:11:59 +0100

    finished first test & module

--- serial/headers/io/io_variables.h
+++ serial/headers/io/io_variables.h
@@ -1,8 +1,10 @@
 #ifndef hpc_wally_io_variables
 #define hpc_wally_io_variables
 
+  #include <iostream>
   #include <string>
   #include <map>
+  #include <fstream>
 
   class IO_Variables {
     private:
@@ -13,7 +15,7 @@
       bool save_print_to_file, save_display_to_file;
       bool load_puzzle_from_file, load_config_from_file, load_results_from_file;
       std::string print_output_filename, display_output_filename;
-      std::string puzzle_image_filename, loaded_results_filename, config_filename;
+      std::string puzzle_image_filename, config_filename, load_results_filename;
     public:
       void set_number_of_final_results(int result_count);
       void set_weighting(std::string pattern_name, int weighting);
@@ -22,11 +24,16 @@
       void set_verbose_print(bool should_print_verbose);
       void set_verbose_display(bool should_display_verbose);
       void set_save_print_to_file(bool should_save_print_to_file);
-      void set_save_print_to_file(std::string filename);
-      void set_print_output_filename(bool should_save_display_to_file);
+      void set_save_display_to_file(bool should_save_print_to_file);
+      void set_load_puzzle_from_file(bool should_load_puzzle_from_file);
+      void set_load_config_from_file(bool should_load_config_from_file);
       void set_load_results_from_file(bool should_load_results_from_file);
+      void set_print_output_filename(std::string filename);
       void set_display_output_filename(std::string filename);
-
+      void set_puzzle_image_filename(std::string filename);
+      void set_config_filename(std::string filename);
+      void set_load_results_filename(std::string filename);
+ 
       int get_number_of_final_results();
       int get_weighting(std::string);
       bool get_print_results(), get_display_results();
@@ -34,7 +41,7 @@
       bool get_save_print_to_file(), get_save_display_to_file();
       bool get_load_puzzle_from_file(), get_load_config_from_file(), get_load_results_from_file();
       std::string get_print_output_filename(), get_display_output_filename();
-      std::string get_puzzle_image_filename(), get_loaded_results_filename(), get_config_filename();
+      std::string get_puzzle_image_filename(), get_config_filename(), get_load_results_filename();
 
       IO_Variables();
   };
--- serial/source/io/io_variables.cpp
+++ serial/source/io/io_variables.cpp
@@ -1,120 +1,195 @@
 #include "../../headers/io/io_variables.h"
-
 using namespace std;
 
 IO_Variables::IO_Variables() {
+  number_of_final_results = 3;
+  print_results = true;
+  display_results = true;
+  verbose_print = false;
+  verbose_display = false;
+  save_print_to_file = false;
+  save_display_to_file = false;
+  load_puzzle_from_file = true;
+  load_config_from_file = false;
+  load_results_from_file = false;
+  print_output_filename = "";
+  display_output_filename = "";
+  puzzle_image_filename = "";
+  config_filename = "";
+  load_results_filename = "";
+  
 }
 
 void IO_Variables::set_number_of_final_results(int result_count){
+  if(result_count > 0) {
+    number_of_final_results = result_count;
+  } else {
+    cerr << "Error: attempted to set 'number_of_final_results' to a value less than or equal to 0. Continuing with previous value" << endl;
+  }
 }
 
 void IO_Variables::set_weighting(string pattern_name, int weighting){
+  if(weighting >=0 ) {
+    pattern_weighting[pattern_name] = weighting;
+  } else {
+    cerr << "Error: attempted to set a 'pattern_weighting' to a value less than 0. Ignoring addition." << endl;
+  }
 }
 
 void IO_Variables::set_print_results(bool should_print){
+  print_results = should_print;
 }
 
 void IO_Variables::set_display_results(bool should_display){
+  display_results = should_display;
 }
 
 void IO_Variables::set_verbose_print(bool should_print_verbose){
+  verbose_print = should_print_verbose;
 }
 
 void IO_Variables::set_verbose_display(bool should_display_verbose){
+  verbose_display = should_display_verbose;
 }
 
 void IO_Variables::set_save_print_to_file(bool should_save_print_to_file){
+  save_print_to_file = should_save_print_to_file;
 }
 
-void IO_Variables::set_save_print_to_file(string filename){
+void IO_Variables::set_save_display_to_file(bool should_save_print_to_file){
+  save_display_to_file = should_save_print_to_file;
 }
 
-void IO_Variables::set_print_output_filename(bool should_save_display_to_file){
+void IO_Variables::set_load_puzzle_from_file(bool should_load_puzzle_from_file){
+  if(load_puzzle_from_file != should_load_puzzle_from_file) { 
+    if(should_load_puzzle_from_file == true ) {
+      cerr << "\t Setting 'load_results_from_file' to false and 'load_puzzle_from_file' to true." << endl;
+    } else { 
+      cerr << "\t Setting 'load_results_from_file' to true and 'load_puzzle_from_file' to false." << endl;
+    }
+  }
+  load_puzzle_from_file = should_load_puzzle_from_file;
+  load_results_from_file = !should_load_puzzle_from_file;
+}
+
+void IO_Variables::set_load_config_from_file(bool should_load_config_from_file){
+  load_config_from_file = should_load_config_from_file;
 }
 
 void IO_Variables::set_load_results_from_file(bool should_load_results_from_file){
+  if(load_results_from_file != should_load_results_from_file) { 
+    if(should_load_results_from_file == true ) {
+      cerr << "\t Setting 'load_results_from_file' to true and 'load_puzzle_from_file' to false." << endl;
+    } else { 
+      cerr << "\t Setting 'load_results_from_file' to false and 'load_puzzle_from_file' to true." << endl;
+    }
+  }
+  load_results_from_file = should_load_results_from_file;
+  load_puzzle_from_file = !should_load_results_from_file;
+}
+
+void IO_Variables::set_print_output_filename(string filename){
+  print_output_filename = filename;
 }
 
 void IO_Variables::set_display_output_filename(string filename){
+  display_output_filename = filename;
+}
+
+void IO_Variables::set_puzzle_image_filename(string filename){
+  fstream check_puzzle;
+  check_puzzle.open(filename.c_str());
+  if(check_puzzle.good() || filename == "") {
+    puzzle_image_filename = filename;
+  } else {
+   cerr << "Error: attempted to set 'puzzle_image_filename' to a file that does not exist. Continuing with previous value." << endl; 
+  }
+  check_puzzle.close();
+}
+
+void IO_Variables::set_config_filename(string filename){
+  fstream check_config;
+  check_config.open(filename.c_str());
+  if(check_config.good() || filename == "") {
+    config_filename = filename;
+  } else {
+   cerr << "Error: attempted to set 'config_filename' to a file that does not exist. Continuing with previous value." << endl; 
+  }
+  check_config.close();
+}
+
+void IO_Variables::set_load_results_filename(string filename){
+  fstream check_load_results;
+  check_load_results.open(filename.c_str());
+  if(check_load_results.good() || filename == "") {
+    load_results_filename = filename;
+  } else {
+   cerr << "Error: attempted to set 'load_results_filename' to a file that does not exist. Continuing with previous value." << endl; 
+  }
+  check_load_results.close();
 }
 
 int IO_Variables::get_number_of_final_results() {
-  int placeholder = -1;
-  return placeholder;
+  return number_of_final_results;
 }
 
-int IO_Variables::get_weighting(string){
-  int placeholder = -1;
-  return placeholder;
+int IO_Variables::get_weighting(string pattern_name){
+  return pattern_weighting[pattern_name];
 }
 
 bool IO_Variables::get_print_results(){
-  bool placeholder = 0;
-  return placeholder;
+  return print_results;
 }
 
 bool IO_Variables::get_display_results(){
-  bool placeholder = 0;
-  return placeholder;
+  return display_results;
 }
 
 bool IO_Variables::get_verbose_print(){
-  bool placeholder = 0;
-  return placeholder;
+  return verbose_print;
 }
 
 bool IO_Variables::get_verbose_display(){
-  bool placeholder = 0;
-  return placeholder;
+  return verbose_display;
 }
 
 bool IO_Variables::get_save_print_to_file(){
-  bool placeholder = 0;
-  return placeholder;
+  return save_print_to_file;
 }
 
 bool IO_Variables::get_save_display_to_file(){
-  bool placeholder = 0;
-  return placeholder;
+  return save_display_to_file;
 }
 
 bool IO_Variables::get_load_puzzle_from_file(){
-  bool placeholder = 0;
-  return placeholder;
+  return load_puzzle_from_file;
 }
 
 bool IO_Variables::get_load_config_from_file(){
-  bool placeholder = 0;
-  return placeholder;
+  return load_config_from_file;
 }
 
 bool IO_Variables::get_load_results_from_file(){
-  bool placeholder = 0;
-  return placeholder;
+  return load_results_from_file;
 }
 
 string IO_Variables::get_print_output_filename(){
-  string placeholder = "err_undefined";
-  return placeholder;
+  return print_output_filename;
 }
 
 string IO_Variables::get_display_output_filename(){
-  string placeholder = "err_undefined";
-  return placeholder;
+  return display_output_filename;
 }
 
 string IO_Variables::get_puzzle_image_filename(){
-  string placeholder = "err_undefined";
-  return placeholder;
+  return puzzle_image_filename;
 }
 
-string IO_Variables::get_loaded_results_filename(){
-  string placeholder = "err_undefined";
-  return placeholder;
+string IO_Variables::get_load_results_filename(){
+  return load_results_filename;
 }
 
 string IO_Variables::get_config_filename(){
-  string placeholder = "err_undefined";
-  return placeholder;
+  return config_filename;
 }
 
