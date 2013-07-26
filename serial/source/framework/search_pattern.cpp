e7d0eddd2b5d962d6ccd2b6034139a888467b4ce
Cian Booth <thisiscian@gmail.com>
2013-07-26 15:28:10 +0100

    findfeatures update

--- serial/source/framework/search_pattern.cpp
+++ serial/source/framework/search_pattern.cpp
@@ -17,6 +17,19 @@ const bool operator==(const Pattern_Result &lhs, const Pattern_Result &rhs) {
   return check;
 }
 
+const bool operator>(const Pattern_Result &lhs, const Pattern_Result &rhs) {
+  bool check = true;
+  Pattern_Information li = lhs.info;
+  Pattern_Information ri = rhs.info;
+
+  check &= (li == ri);
+  check &= (lhs.wally_location[0] == rhs.wally_location[0]);
+  check &= (lhs.wally_location[1] == rhs.wally_location[1]);
+  check &= (lhs.scale[0] == rhs.scale[0]);
+  check &= (lhs.scale[1] == rhs.scale[1]);
+  check &= (lhs.certainty > rhs.certainty);
+  return check;
+}
 const bool operator!=(const Pattern_Result &lhs, const Pattern_Result &rhs) {
   return !(lhs == rhs);
 }
