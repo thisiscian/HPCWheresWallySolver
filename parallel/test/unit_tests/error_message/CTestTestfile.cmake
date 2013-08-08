7bcea34786cef952529ff8a4e15f15266d1b3338
thisiscian <thisiscian@gmail.com>
2013-08-08 12:30:02 +0100

    removed Ctest files

--- parallel/test/unit_tests/error_message/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cian/Project/HPCWheresWallySolver/serial/test/unit_tests/error_message
-# Build directory: /home/cian/Project/HPCWheresWallySolver/serial/test/unit_tests/error_message
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test_error_message_class "ww_ut_error_message")
--- parallel/test/unit_tests/pattern_information/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cian/Project/HPCWheresWallySolver/serial/test/unit_tests/pattern_information
-# Build directory: /home/cian/Project/HPCWheresWallySolver/serial/test/unit_tests/pattern_information
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test_pattern_information_class "ww_ut_pattern_information")
